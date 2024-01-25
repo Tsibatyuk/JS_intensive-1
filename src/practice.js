af8af76a38844a1e3e38e6e409207f5029cace05
Tsibatyuk <149365683+ZhekaTsibatyuk@users.noreply.github.com>
2024-01-25 15:28:11 +0200

    first commit

---
+++ src/practice.js
@@ -0,0 +1,38 @@
+//undifined variables
+console.log(typeof a)
+
+let stringForTest = 'Wellcome to club budy'
+
+console.log( stringForTest)
+console.log(typeof stringForTest)
+
+//change data type to Number
+stringForTest = 10
+console.log(stringForTest)
+console.log(typeof stringForTest)
+
+//change data type to BigIn
+stringForTest = 10n
+console.log(stringForTest)
+console.log(typeof stringForTest)
+
+//create boolean variable
+stringForTest = 10; const numberTen = '10';
+console.log(stringForTest, numberTen)
+console.log(typeof stringForTest)
+console.log(numberTen)
+console.log(typeof numberTen)
+let booleanType1 = stringForTest == numberTen
+console.log(booleanType1)
+let booleanType2 = stringForTest === numberTen
+console.log(booleanType2)
+
+//create NULL variable
+stringForTest = null
+console.log(stringForTest)
+console.log(typeof stringForTest)
+
+//create symbol variable
+stringForTest = Symbol()
+console.log(stringForTest)
+console.log(typeof stringForTest)
